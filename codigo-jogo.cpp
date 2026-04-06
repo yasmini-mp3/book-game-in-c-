2a94dbdb017f6ec954f02fa82b97710a1e029737
Th1-ag0 <thiagopassos2808@gmail.com>
2026-04-06 00:24:48 -0300

    faltou ponto e vírgula

--- codigo-jogo.cpp
+++ codigo-jogo.cpp
@@ -285,7 +285,7 @@ int main()
         bool serrote = true;
         int acaoBatalha;         // organizar as ações do jogador durante a batalha
         if (faca == true){
-            danoProta = 2 + aliados // recalcula COM os aliados que foram coletados na historia, cada aliado contribui com +1 de dano
+            danoProta = 2 + aliados; // recalcula COM os aliados que foram coletados na historia, cada aliado contribui com +1 de dano
         }   
         else{
             danoProta = 1 + aliados;
